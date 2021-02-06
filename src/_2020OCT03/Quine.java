e1a2d456854fb00b71875b30b1aeb9bf408a3f16
Leo Irakliotis <lgreco@gmail.com>
2021-02-05 21:11:37 -0600

    SJC cleanup 2021

---
+++ src/_2020OCT03/Quine.java
@@ -0,0 +1,13 @@
+package _2020OCT03;
+
+public class Quine {
+    public static void main(String[] args) {
+        char c=34; System.out.println(s+c+s+c+';'+'}');
+    } static
+
+    String s="public class Quine {\n" +
+            "    public static void main(String[] args) {\n" +
+            "        char c=34; System.out.println(s+c+s+c+';'+'}');\n" +
+            "    } static";
+
+}
